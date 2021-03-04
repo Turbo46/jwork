3f129faff650f1bbc50c0a852c3f49e1104e11b3
Gilbert Siagian <torumitsuki99@gmail.com>
2021-03-04 18:24:44 +0700

    Modul 1 -Case Study -Jworks

---
+++ jwork.java
@@ -0,0 +1,14 @@
+
+/**
+ * Abstract class jwork - write a description of the class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class jwork
+{
+    public void main (String[] args)
+    {
+        
+    }
+}
