4941c869f8ee164230d160c3b2125c37345f92a2
Gilbert Siagian <torumitsuki99@gmail.com>
2021-04-21 16:17:30 +0700

    Pre-CS -Migrasi IDE

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"DatabaseJob"},{"p":"<Unnamed>","l":"DatabaseRecruiter"},{"p":"<Unnamed>","l":"Invoice"},{"p":"<Unnamed>","l":"Job"},{"p":"<Unnamed>","l":"Jobseeker"},{"p":"<Unnamed>","l":"jwork"},{"p":"<Unnamed>","l":"Location"},{"p":"<Unnamed>","l":"Recruiter"}]
