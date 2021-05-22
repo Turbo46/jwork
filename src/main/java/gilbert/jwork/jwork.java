4a79256cf4157c2e8bf92a768db56a6e515bc7f6
Gilbert Siagian <torumitsuki99@gmail.com>
2021-05-22 10:04:36 +0700

    Modul 8 -Post Test -Revisi

--- src/main/java/gilbert/jwork/jwork.java
+++ src/main/java/gilbert/jwork/jwork.java
@@ -2,7 +2,6 @@ package gilbert.jwork;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.ArrayList;
-import static jdk.javadoc.internal.doclets.formats.html.markup.Head.calendar;
 
 @SpringBootApplication
 public class jwork {
@@ -14,9 +13,17 @@ public class jwork {
             DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1,"Back End Tokopedia", DatabaseRecruiter.getRecruiterById(DatabaseRecruiter.getLastId()), 500000, JobCategory.BackEnd));
             DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1,"Front End Shopee", DatabaseRecruiter.getRecruiterById(DatabaseRecruiter.getLastId()), 150000, JobCategory.FrontEnd));
 
-            DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId()+1, "Gilbert Siagian", "gilbertsiagian@gmail.com", "Gibu123", calendar));
-            DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId()+1, "Gilbert", "gilbertgibu@gmail.com", "Gibu456", 2019, 3, 26));
-            DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId()+1, "Titus", "titus@gmail.com", "Titus123"));
+            //DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId()+1, "Gilbert", "gilbertgibu@gmail.com", "Gibu456", 2021, 5, 22));
+            //DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId()+1, "Titus", "titus@gmail.com", "Titus123"));
+
+            ArrayList<Job> job1 = new ArrayList<Job>();
+            job1.add(DatabaseJob.getJobById(1));
+            job1.add(DatabaseJob.getJobById(2));
+
+            //DatabaseInvoice.addInvoice(new EwalletPayment(DatabaseInvoice.getLastId()+1, job1, DatabaseJobseeker.getJobseekerById(1), DatabaseBonus.getBonusByReferralCode("UIDGS2")));
+            //DatabaseInvoice.addInvoice(new EwalletPayment(DatabaseInvoice.getLastId()+1, job1, DatabaseJobseeker.getJobseekerById(2), DatabaseBonus.getBonusByReferralCode("UIDGS2")));
+
+
             SpringApplication.run(jwork.class, args);
     }
 }
