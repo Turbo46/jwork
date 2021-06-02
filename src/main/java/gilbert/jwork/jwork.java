9c235744184ce581b5711b2991829d71b00a69f8
Turbo46 <torumitsuki99@gmail.com>
2021-06-02 14:30:36 +0700

    Modul 9 -Revisi

--- src/main/java/gilbert/jwork/jwork.java
+++ src/main/java/gilbert/jwork/jwork.java
@@ -1,29 +1,27 @@
 package gilbert.jwork;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class jwork {
-        public static void main(String[] args) throws RecruiterNotFoundException, JobSeekerNotFoundException, JobNotFoundException, OngoingInvoiceAlreadyExistsException, EmailAlreadyExistsException {
 
-            Location location1 = new Location("Banten", "Tangerang Selatan", "Rawabuntu");
-            Location location2 = new Location("Jakarta", "Jakarta Selatan", "Asrama");
-
-            DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1,"Back End Tokopedia", DatabaseRecruiter.getRecruiterById(DatabaseRecruiter.getLastId()), 500000, JobCategory.BackEnd));
-            DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1,"Front End Shopee", DatabaseRecruiter.getRecruiterById(DatabaseRecruiter.getLastId()), 150000, JobCategory.FrontEnd));
-
-            //DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId()+1, "Gilbert", "gilbertgibu@gmail.com", "Gibu456", 2021, 5, 22));
-            //DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId()+1, "Titus", "titus@gmail.com", "Titus123"));
-
-            ArrayList<Job> job1 = new ArrayList<Job>();
-            job1.add(DatabaseJob.getJobById(1));
-            job1.add(DatabaseJob.getJobById(2));
-
-            //DatabaseInvoice.addInvoice(new EwalletPayment(DatabaseInvoice.getLastId()+1, job1, DatabaseJobseeker.getJobseekerById(1), DatabaseBonus.getBonusByReferralCode("UIDGS2")));
-            //DatabaseInvoice.addInvoice(new EwalletPayment(DatabaseInvoice.getLastId()+1, job1, DatabaseJobseeker.getJobseekerById(2), DatabaseBonus.getBonusByReferralCode("UIDGS2")));
-
-
-            SpringApplication.run(jwork.class, args);
+    public static void main(String[] args) {
+        Location location1 = new Location("DKI Jakarta", "Jakarta Selatan", "Asrama");
+        Location location2 = new Location("Banten", "Tangerang Selatan", "Rawabuntu");
+        Location location3 = new Location("Sulawesi Tengah", "Palu", "Sekolah");
+        DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId() + 1, "Gilbert", "gilbert@gmail.com", "177013", location1));
+        DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId() + 1, "Gibu", "gibu@gmail.com", "304321", location2));
+        DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId() + 1, "Titus", "titus@gmail.com", "765321", location3));
+        try {
+            DatabaseJob.addJob(new Job(1, "Back End Engineer Tokopedia", DatabaseRecruiter.getRecruiterById(1), 10000, JobCategory.BackEnd));
+            DatabaseJob.addJob(new Job(2, "Front End Engineer Lazada", DatabaseRecruiter.getRecruiterById(2), 30000, JobCategory.FrontEnd));
+            DatabaseJob.addJob(new Job(3, "UX Researcher Shopee", DatabaseRecruiter.getRecruiterById(3), 20000, JobCategory.UX));
+            DatabaseJob.addJob(new Job(4, "UI Designer Bukalapak", DatabaseRecruiter.getRecruiterById(3), 50000, JobCategory.UI));
+        }
+        catch(RecruiterNotFoundException e){
+            e.printStackTrace();
+        }
+        SpringApplication.run(jwork.class, args);
     }
+
 }
